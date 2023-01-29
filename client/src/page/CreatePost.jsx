a428d45e20f5a78bffa28e8a2d8e1d48006140cb
Derrick Chan <zchan012@e.ntu.edu.sg>
2023-01-29 14:59:28 +0800

    Update CreatePost.jsx

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -29,7 +29,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "https://dalle-arbb.onrender.com/api/v1/dalle",
+          "https://dall-e-jjc0.onrender.com/api/v1/dalle",
           {
             method: "POST",
             headers: {
@@ -60,7 +60,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "https://dalle-arbb.onrender.com/api/v1/post",
+          "https://dall-e-jjc0.onrender.com/api/v1/post",
           {
             method: "POST",
             headers: {
